9635553faf54b7864062ea8d3c8b4b779ba86b4e
Ikenna Onuorah <ionuorah@ionuora-ltm2qqv.internal.salesforce.com>
2022-07-14 11:02:10 -0700

    commited 3

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 require("colors")
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
 
 const IS_TESTING = process.env.NODE_ENV === "test"
 
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const authRouter = require("./routes/auth")
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 app.use(morgan("tiny"));
